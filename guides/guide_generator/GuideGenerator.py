c850b5fb00a957febec1ba7a7bad751a860b4232
Sam Clark <sam.clark1142@gmail.com>
2019-09-05 17:03:09 -0400

    guide gen script

--- guides/guide_generator/GuideGenerator.py
+++ guides/guide_generator/GuideGenerator.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup as Soup
+from Account import Account
 
 
 class GuideGenerator:
@@ -7,14 +8,24 @@ class GuideGenerator:
         self.guide_source = open(self.guide_template_path, "r").read()
         self.guide_soup = Soup(self.guide_source)
 
-        print(self.guide_soup)
+        self.accounts = []
 
     def add_account(self, username, password):
-        pass
+        account = Account(username=username, password=password)
+        self.accounts.append(account)
 
     def generate_guides(self, output_dir):
-        pass
+        username_element = self.guide_soup.find("mark", {"id": "username"})
+        password_element = self.guide_soup.find("mark", {"id": "password"})
+
+        for account in self.accounts:
+            username_element.string = account.username
+            password_element.string = account.password
+            open(output_dir + "/" + account.username + ".md", "w").write(str(self.guide_soup))
 
 
 if __name__ == '__main__':
-    generator = GuideGenerator("../")
+    generator = GuideGenerator("../getting_started/StudentGettingStarted.md")
+    generator.add_account("test_username", "test_password")
+    generator.add_account("test_username2", "test_password2")
+    generator.generate_guides(".")
